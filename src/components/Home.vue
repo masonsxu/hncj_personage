b64dd905d77d96d098197f777b9a15911586f9a1
masonsxu <masonsxu@foxmail.com>
2021-03-09 17:39:44 +0800

    将大屏的各个部分写为组件方便调用

---
+++ src/components/Home.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="home">
+    <el-container style="height: 600px; border: 1px solid #eee">
+      <el-header>
+        <span>突发事件知识图谱</span>
+        <el-dropdown
+          style="float: right; font-size: 12px; right: 70px; height: 30px"
+        >
+          <i class="el-icon-setting" style="margin-right: 15px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item @click.native="jump('sign')"
+              >注销</el-dropdown-item
+            >
+            <el-dropdown-item @click.native="jump('github')"
+              >关于我们</el-dropdown-item
+            >
+          </el-dropdown-menu>
+        </el-dropdown>
+        <span style="float: right; text-align: right">{{user}}</span>
+      </el-header>
+      <el-container>
+        <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+          <el-menu router :default-openeds="['3']">
+            <el-submenu
+              v-for="(item, index) in $router.options.routes"
+              :key="index + 1"
+              :index="index + 1 + ''"
+              v-if="item.show"
+            >
+              <template slot="title"
+                ><i class="el-icon-menu"></i>{{ item.name }}</template
+              >
+              <el-menu-item
+                v-for="(item2, index2) in item.children"
+                :key="index2 + 1"
+                :index="item2.path"
+                :class="$route.path == item2.path ? 'is-active' : ''"
+              >
+                {{ item2.name }}
+              </el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-container>
+          <el-main>
+            <router-view></router-view>
+          </el-main>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user:'admin'
+    };
+  },
+  methods: {
+    // getRouter: function () {
+    //   var a = this.$router.options.routes;
+    //   console.log(a);
+    // },
+    jump: function (res) {
+      if (res == "sign") {
+        this.$router.push('/login')
+      } else if (res == "github") {
+        alert("项目地址，欢迎star！");
+        window.open('https://github.com/MasonsxuGroup/Ekg')
+      }
+    },
+  },
+  mounted() {
+    // this.getRouter();
+  },
+  watch: {
+    $route: {
+      handler: function (val, oldVal) {
+        console.log(val);
+      },
+      // 深度观察监听
+      deep: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.el-header {
+  background-color: rgba(36, 55, 67, 1);
+  color: rgba(40, 183, 141, 1);
+  text-align: left;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: rgba(138, 148, 155, 1);
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: rgba(250, 250, 250, 1);
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+</style>
---
+++ src/components/Recognition.vue
@@ -0,0 +1,185 @@
+<template>
+  <div id="entity-reco">
+    <el-row id="top-row">
+      <i class="el-icon-s-home"></i>
+      <span>实体识别</span>
+    </el-row>
+    <el-row id="mid-row">
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+        <p id="mid-form-label">输入信息</p>
+        <div style="padding: 0 15px">
+          <el-form-item prop="content">
+            <el-input
+              type="textarea"
+              v-model="ruleForm.content"
+              rows="4"
+              maxlength="150"
+              show-word-limit
+            ></el-input>
+            <el-form-item style="margin-top: 10px">
+              <el-button type="primary" @click="submitForm('ruleForm')"
+                >提交</el-button
+              >
+              <el-button @click="resetForm">重置</el-button>
+            </el-form-item>
+          </el-form-item>
+        </div>
+      </el-form>
+    </el-row>
+    <el-row id="bottom-row">
+      <p id="btm-form-label">识别结果</p>
+      <div
+        style="
+          padding: 0 15px;
+          border-style: grrove;
+          overflow: hidden;
+          display: flex;
+          flex-wrap: wrap;
+        "
+      >
+        <div id="output-result" -->
+          >
+          <p
+            style="
+              margin: 0;
+              line-height: 30px;
+              font-weight: 580;
+              padding: 0 16px;
+            "
+          >
+            {{ entity }}
+          </p>
+          <p
+            style="
+              margin: 0;
+              line-height: 20px;
+              font-size: 12px;
+              padding: 0 16px;
+            "
+          >
+            {{ resp_values[index] }}
+          </p>
+        </div>
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {
+      isActive: "",
+      rules: {
+        content: [
+          { required: true, message: "输入内容不能为空！", trigger: "blur" },
+          {
+            min: 0,
+            max: 150,
+            message: "输入字符不得超过150个字符",
+            trigger: "blur"
+          }
+        ]
+      },
+      entities: [],
+      resp_values: [],
+      result: ""
+    };
+  },
+  computed: {
+    ...mapState({
+      ruleForm: state => state.ruleForm
+    })
+  },
+  methods: {
+    submitForm(formName) {
+      const _this = this;
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.axios
+            .post("/api/extract", this.ruleForm)
+            .then(function(res) {
+              let resp = res.data.data;
+              let temp = [];
+              let value_temp = [];
+              if (res.status == 200) {
+                for (let i in resp) {
+                  temp.push(String(resp[i]["item"]));
+                  _this.entities = temp;
+                  value_temp.push(String(resp[i]["pos"]));
+                  _this.resp_values = value_temp;
+                }
+              }
+            })
+            .catch(error => console.log(error));
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm: function() {
+      this.ruleForm.content = "";
+    },
+    onMouseOver: function(index) {
+      this.isActive = index;
+    },
+    onMouseLeave: function() {
+      this.isActive = "";
+    }
+  },
+  created() {},
+  mounted() {}
+};
+</script>
+
+<style lang="scss" scoped>
+#top-row {
+  margin-bottom: 15px;
+  height: 60px;
+  line-height: 60px;
+  text-align: left;
+  padding-left: 20px;
+  background-color: rgba(138, 148, 155, 0.2);
+}
+
+#mid-row {
+  height: 200px;
+  border: 1px solid rgba(138, 148, 155, 0.2);
+  margin-bottom: 15px;
+  overflow-y: hidden;
+}
+.el-textarea >>> .el-textarea__inner {
+  border-style: ridge;
+}
+#bottom-row {
+  border: 1px solid rgba(138, 148, 155, 0.2);
+}
+#mid-form-label,
+#btm-form-label {
+  height: 20px;
+  line-height: 20px;
+  text-align: left;
+  padding: 0 15px;
+  font-weight: 600;
+  margin: 10px 0;
+}
+
+#output-result {
+  border: 1px solid #e0e0e0;
+  border-radius: 4%;
+  background-color: white;
+  overflow: hidden;
+  font-size: 15px;
+  margin: 5px;
+  height: 48px;
+}
+.activeClass {
+  background-color: #409eff !important;
+}
+#outpu-result p {
+  line-height: 20;
+  position: relative;
+}
+</style>
